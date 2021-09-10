79c69c6e68da8ebd345c7460d0869975f45d649f
Siddharth Anand Srivastav <siddharth.asrivastav.ece19@itbhu.ac.in>
2021-09-11 01:33:55 +0530

    trackbars + left right click added

--- simplegui.py
+++ simplegui.py
@@ -1,9 +1,19 @@
 import PySimpleGUI as sg
 import os.path
 from PIL import Image, ImageTk
+import numpy as np
 import io
+import cv2
+import pipeprocess
 
+name = 'Control'
+global p1
+global p2
+global minr
+global maxr
 # First the window layout in 2 columns
+ct = 0
+
 def get_img_data(f, maxsize=(2000, 1080), first=False):
     """Generate image data using PIL
     """
@@ -15,6 +25,71 @@ def get_img_data(f, maxsize=(2000, 1080), first=False):
         del img
         return bio.getvalue()
     return ImageTk.PhotoImage(img)
+
+def on_trackbar1(minradius):
+    global ct
+    minr = minradius
+    maxr = cv2.getTrackbarPos('maximum_radius', name)
+    p1 = cv2.getTrackbarPos('param1', name)
+    p2 = cv2.getTrackbarPos('param2', name)
+    ct = pipeprocess.preprocess(minr,maxr , p1,p2)
+def on_trackbar2(maxradius):
+    global ct
+    maxr = maxradius
+    minr = cv2.getTrackbarPos('minimum_radius', name)
+    p1 = cv2.getTrackbarPos('param1', name)
+    p2 = cv2.getTrackbarPos('param2', name)
+    ct = pipeprocess.preprocess(minr,maxr, p1,p2)
+def on_trackbar3(par1):
+    global ct
+    p1 = par1
+    maxr = cv2.getTrackbarPos('maximum_radius', name)
+    minr = cv2.getTrackbarPos('minimum_radius', name)
+    p2 = cv2.getTrackbarPos('param2', name)
+    ct = pipeprocess.preprocess(minr,maxr , p1,p2)
+def on_trackbar4(par2):
+    global ct
+    p2 = par2
+    maxr = cv2.getTrackbarPos('maximum_radius', name)
+    minr = cv2.getTrackbarPos('minimum_radius', name)
+    p1 = cv2.getTrackbarPos('param1', name)
+    ct = pipeprocess.preprocess(minr,maxr , p1,p2)
+
+def drawcircle(action, x, y, flags, *userdata):
+    global ct
+    img = cv2.imread('aa.jpg')
+    cv2.imshow(name, img)
+    if action == cv2.EVENT_LBUTTONDOWN:
+        centre = (x,y)
+        ct = ct+1
+        cv2.circle(img , centre, 15, (0, 200, 0), 2)
+        print("Count = ", ct)
+    elif action == cv2.EVENT_RBUTTONDOWN:
+        ct = ct-1
+        centre = (x,y)
+        cv2.circle(img , centre, 15, (0, 0, 255), 2)
+        print("Count = ", ct)
+    cv2.imshow(name, img)
+    cv2.imwrite('aa.jpg', img)
+    
+def opencv_window(file_name):
+    button = [20,60,50,250]
+    cv2.namedWindow(name,cv2.WINDOW_NORMAL)
+##    cv2.resizeWindow(name, 1080, 720)
+    control_image = np.zeros((80,300), np.uint8)
+    control_image[button[0]:button[1],button[2]:button[3]] = 180
+    cv2.putText(control_image, 'Button',(100,50),cv2.FONT_HERSHEY_PLAIN, 2,(0),3)
+
+    #show 'control panel'
+    cv2.createTrackbar('minimum_radius', name , 1, 100, on_trackbar1)
+    cv2.createTrackbar('maximum_radius', name , 30, 100, on_trackbar2)
+    cv2.createTrackbar('param1', name , 140, 250, on_trackbar3)
+    cv2.createTrackbar('param2', name , 25, 250, on_trackbar4)
+    cv2.setMouseCallback(name,drawcircle)
+##    cv2.imshow(name, control_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 file_list_column = [
     [
@@ -48,7 +123,6 @@ layout = [
 ]
 
 window = sg.Window("Image Viewer", layout)
-win2_active= False
 # Run the Event Loop
 while True:
     event, values = window.read()
@@ -80,19 +154,8 @@ while True:
 
         except:
             pass
-    if event == 'Proceed'  and not win2_active:
-        win2_active = True
-        layout2 = [[sg.Text('Window 2')],       # note must create a layout from scratch every time. No reuse
-                   [sg.Button('Exit')],
-                   [sg.Image(data=get_img_data(filename, first=True))]]
-        win2 = sg.Window('Window 2', layout2)
-        while True:
-            ev2, vals2 = win2.read()
-            if ev2 == sg.WIN_CLOSED or ev2 == 'Exit':
-                win2.close()
-                win2_active = False
-                window.UnHide()
-                break
+    if event == 'Proceed':
+        opencv_window(filename)
                                     
 ##    print(event)
 window.close()
